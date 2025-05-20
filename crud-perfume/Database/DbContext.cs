173d9d482b4dc206c456e3eea496def2e05436a2
Maria Cecília Araújo <mclemos65@gmail.com>
2025-05-20 12:34:04 -0300

    .

--- crud-perfume/Database/DbContext.cs
+++ crud-perfume/Database/DbContext.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace crud_perfume.Database
 {
     public class DbContext
@@ -23,6 +24,42 @@ namespace crud_perfume.Database
                 _dbConection = new SQLiteAsyncConnection(databasePath, Flags);
                 _dbConection.CreateTableAsync<Models.Perfume>();
             }
+        }
+        public async Task Initialize()
+        {
+            await _dbConection.CreateTableAsync<Models.Perfume>();
+        }
+
+        public List<T> GetTableRows (string tableName) where T : class
+        {
+            object[] obj = new object[] { };
+            TableMapping map  = new TableMapping(Type.GetType(nameSpace + tableName);
+            string query = "SELECT * FROM ["+ tableName +"]" ;
+            return _dbConection.QueryAsync(map, query, obj).Result.Cast<T>.ToList();
+        }
+      
+        public object GetTableRow(string tableNmae, string colum, string value)
+        {
+            object[] obj = new object[] { };
+            TableMapping map = new TableMapping(Type.GetType(nameSpace + tableNmae));
+            string query = "SELECT * FROM [" + tableNmae + "] WHERE [" + colum + "] = " + value;
+            return _dbConection.QueryAsync(map, query, obj).Result.Cast<T>.ToList();
+        }
+        public async Task<int> CreateAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            return await _dbConection.InsertAsync(entity);
+        }
+        public async Task<int> UpdateAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            return await _dbConection.UpdateAsync(entity);
+        }
+        public async Task<int> DeleteAsync<TEntity>(TEntity entity) where TEntity : class
+        {
+            return await _dbConection.DeleteAsync(entity);
+        }
+        public async Task<int> ArrOrUpdate<TEntity>(TEntity entity) where TEntity : class
+        {
+            return await _dbConection.InsertOrReplaceAsync(entity);
         }
     }
 }
